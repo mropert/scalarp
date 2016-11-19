42d1f625da47bbb3ae2f727cec8e159d2cdfe5b2
mro <mro@puchiko.net>
2016-11-19 21:30:56 +0100

    Simple character login + sheet. Closes #2.

---
+++ app/src/js/model/Character.jsx
@@ -0,0 +1,34 @@
+class Character {
+  static get(charId) {
+    if (charId == "01234567") {
+      return {
+        id: charId,
+        name: "Prof Schmallock",
+        race: "Human",
+        class: "Wizard",
+        skills: "Theory (9001), Practice (0)"
+      }
+    }
+    else if (charId == "11111111") {
+      return {
+        id: charId,
+        name: "Titine",
+        race: "Drow",
+        class: "Rogue",
+        skills: "Backstab (5), Luck(-INT_MAX), Sneak (4), Thievery (2)"
+      }
+    }
+    else if (charId == "deadbeef") {
+      return {
+        id: charId,
+        name: "Grudu",
+        race: "Half-orc",
+        class: "Barbarian",
+        skills: "Hurt (4), Kill (5), Maim (3), Rage (6), Two-handed (4)"
+      }
+    }
+    return null;
+  }
+}
+
+module.exports = Character;
