4a467673338ef997ee4fb97d10d4e8a38836db94
mro <mro@puchiko.net>
2016-12-01 22:34:57 +0100

    Fix warnings

--- app/src/js/component/CharacterInfo.jsx
+++ app/src/js/component/CharacterInfo.jsx
@@ -1,18 +1,16 @@
 import React from 'react';
 
-class CharacterInfo extends React.Component {
-  render() {
-    const char = this.props.character;
-    return (<div>
-      <h1>Character sheet</h1>
-      <ul>
-        <li>Id: #{char.id}</li>
-        <li>Name: {char.name}</li>
-        <li>Race: {char.race}</li>
-        <li>Skills: {char.skills}</li>
-      </ul>
-    </div>);
-  }
+function CharacterInfo(props) {
+  const char = props.character;
+  return (<div>
+    <h1>Character sheet</h1>
+    <ul>
+      <li>Id: #{char.id}</li>
+      <li>Name: {char.name}</li>
+      <li>Race: {char.race}</li>
+      <li>Skills: {char.skills}</li>
+    </ul>
+  </div>);
 }
 
 CharacterInfo.propTypes = {
