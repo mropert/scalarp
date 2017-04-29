70efc7f137a894d5343e1ade93644df92a585d7b
Mathieu Ropert <mro@puchiko.net>
2017-04-29 19:11:32 +0200

    Fix mocha tests

--- app/src/test/scalarp.jsx
+++ app/src/test/scalarp.jsx
@@ -1,3 +1,8 @@
+/* eslint-env mocha */
 import { assert } from 'chai';
 
-assert.equal(true, true, 'Dummy test');
+describe('Dummy test', () => {
+  it('should be equal to true', () => {
+    assert.equal(true, true);
+  });
+});
